4fcd05bf48e129174b4c5bd4620813ac7469cf2b
carolleps <carolleps@gmail.com>
2016-10-29 21:35:45 +1100

    Sass

--- Udacity-nanodegree/frontend-nanodegree-arcade-game-master/README.md
+++ Udacity-nanodegree/frontend-nanodegree-arcade-game-master/README.md
@@ -1,5 +1,17 @@
 frontend-nanodegree-arcade-game
 ===============================
+It's swimming time! Guide the princess to the water. But watch out for the bugs! Don't let them hit her. 
+
+How to play
+===========
+1. Open the index.html file in a browser of your choice.
+2. Avoid collision pressing "up","down", "right" and "left" keys to make the player move in the respective directions
+3. Once the player reaches water, the counter at the top will increase.
+4. The game will reset everytime there's a collision between player and a bug.
+
+About the project
+=================
+This Arcade game clone is part of the content of [Udacity's Front-end Web Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001).
 
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
--- Udacity-nanodegree/frontend-nanodegree-arcade-game-master/js/app.js
+++ Udacity-nanodegree/frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,8 +1,18 @@
+//global variables - will be helpful to generate random positions and speed to the enemies
+// possibleX will delay the new enemy appearance
+// possibleY only allow Enemies apear at the stone/street blocks
+var possibleX = [-100, -200, -250];
+var possibleY = [60, 145, 230];
+var possibleSp = [55, 80, 100, 130];
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = possibleX[Math.floor(Math.random() * possibleX.length)];
+    this.y = possibleY[Math.floor(Math.random() * possibleY.length)];
+    this.speed = possibleSp[Math.floor(Math.random() * possibleSp.length)];
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +24,22 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    
+    //After the canvas limit, the Enemy will return to the position -100 
+    if(this.x > 505) {
+        this.x = -100;
+        this.y = possibleY[Math.floor(Math.random() * possibleY.length)];
+        this.speed = possibleSp[Math.floor(Math.random() * possibleSp.length)];
+    }
+
+    // check for collision. If it happens, player returns to the respawn position
+    if (this.y == player.y && (this.x > player.x - 55 && this.x < player.x + 55)) {
+        player.x = 200;
+        player.y = 400;
+        player.saved = 1;
+        document.getElementsByClassName("points")[0].innerHTML='Oh-oh.. Let\'s try that again';
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +50,75 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 200;
+    this.y = 400;
+    this.sprite = 'images/char-princess-girl.png'
+    // will be counting how many times the player wins
+    this.saved = 1;
+}
 
+Player.prototype.update = function() {
+
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//execute this moves according to the keyCode
+Player.prototype.handleInput = function(keyCode) {
+    if (keyCode == 'up'){
+        if(this.y <= 60){
+            this.x = 200;
+            this.y = 400;
+            points = this.saved++;
+            document.getElementsByClassName("points")[0].innerHTML='Are you wet? '+ points;
+        }
+        else{
+            this.y = this.y - 85;
+        }
+    }
+
+    if (keyCode == 'down'){
+        if(this.y >= 400){
+            this.y = this.y;
+        }
+        else {
+         this.y = this.y + 85;
+        }
+
+    }
+    if (keyCode == 'right'){
+        if(this.x >= 400){
+            this.x = this.x;
+        }
+        else{
+        this.x = this.x + 50;
+        }
+    }
+
+    if (keyCode == 'left'){
+       if(this.x <=0){
+            this.x = this.x;
+        }
+        else{
+        this.x = this.x - 50;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];
+for (i=0; allEnemies.length <=5; i++){
+    var bug = new Enemy();
+    allEnemies.push(bug);
+}
 
+// Place the player object in a variable called player
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
